de047e20ff506fc0f46245eafbcdf1014786e531
ebisadugna <130274241+ebisadugna@users.noreply.github.com>
2025-08-06 23:30:33 +0300

    finish

--- app/api/order/list/route.js
+++ app/api/order/list/route.js
@@ -3,6 +3,7 @@ import Address from "@/models/Address";
 import Order from "@/models/Order";
 import Product from "@/models/Product";
 import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 export async function GET(request) {
   try {
@@ -13,7 +14,7 @@ export async function GET(request) {
     Address.length;
     Product.length;
 
-    const orders = await Order.find({ userId }).populate('address items.product');
+    const orders = await Order.find({userId}).populate('address items.product');
 
     return NextResponse.json({ success: true, orders });
 } catch (error) {
